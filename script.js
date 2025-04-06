f5658f87b7a80d84d0ae64ab2db797b8bd51e7e1
Arman Maurya <armanmarya6@gmail.com>
2025-04-07 01:01:53 +0530

    Added Basic Template

--- script.js
+++ script.js
@@ -5,4 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
         backSpeed: 25,
         loop: true
     });
+
+    const tabs = document.querySelectorAll(".tab-btn");
+    const contents = document.querySelectorAll(".tab-content");
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            // Remove active class from all tabs
+            tabs.forEach((t) => t.classList.remove("active-tab"));
+            // Hide all tab contents
+            contents.forEach((content) => content.classList.add("hidden"));
+
+            // Add active class to clicked tab
+            tab.classList.add("active-tab");
+            // Show corresponding tab content
+            const target = document.getElementById(tab.dataset.tab);
+            target.classList.remove("hidden");
+        });
+    });
 });
