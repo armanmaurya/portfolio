bb6e03335abb2167bdca5bedfb8730aa4b9eceb1
Arman Maurya <armanmarya6@gmail.com>
2025-04-06 22:50:16 +0530

    Added Basic Information and maked the website single page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-text", {
+        strings: ["Web Developer", "Designer", "Freelancer"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true
+    });
+});
